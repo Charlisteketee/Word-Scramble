5477d9bfc8349672351a26a060ec3fd359818812
Charlisteketee <charlisteketee@gmail.com>
2024-07-03 10:31:39 -0700

    Start user auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :words
+  get 'game/new', to: 'game#new_game'
+  post 'game/guess', to: 'game#guess'
+  root 'game#new_game'
 end
--- spec/models/game_controller_spec.rb
+++ spec/models/game_controller_spec.rb
@@ -1,14 +1,15 @@
 require 'rails_helper'
 
+
 RSpec.describe GameController, type: :controller do
   before do
     @user = User.create(username: "testeruser", password: "password")
     @word = Word.create(content: "example")
+    sign_in @user
   end
 
   describe "GET #new_game" do
     it "returns http success" do
-      sign_in @user
       get :new_game
       expect(response).to have_http_status(:success)
     end
@@ -16,13 +17,11 @@ RSpec.describe GameController, type: :controller do
 
   describe "POST #guess" do
     it "incremenets the user's score on a correct guess" do
-      sign_in @user
       post :guess, params: { word_id: @word.id, guess: "example" }
       expect(@user.reload.score).to eq(1)
     end
 
     it "does not increment the user's score on an incorrect guess" do
-      sign_in @user
       post :guess, params: { word_id: @word.id, guess: "wrong" }
       expect(@user.reload.score).to eq(0)
     end
